c60fee8ada36bd3efdf39008cc7e7f86438af9f0
Lukáš Andrýsek <lukas.andrysek@gmail.com>
2025-02-14 00:12:58 +0100

    feat: working book Screen

--- src/client/java/com/lucker/mobbuddies/MobBuddiesClient.java
+++ src/client/java/com/lucker/mobbuddies/MobBuddiesClient.java
@@ -1,26 +1,13 @@
 package com.lucker.mobbuddies;
 
-import io.wispforest.lavender.md.features.OwoUIModelFeature;
-import io.wispforest.owo.client.OwoClient;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientEntityEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
 import net.fabricmc.fabric.api.event.player.UseItemCallback;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.render.entity.*;
-import net.minecraft.client.render.entity.model.EntityModelLayer;
-import net.minecraft.client.render.entity.model.EntityModelLayers;
-import net.minecraft.client.render.entity.model.PiglinEntityModel;
-import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.mob.MobEntity;
 import net.minecraft.item.Items;
-import net.minecraft.util.Identifier;
 import net.minecraft.util.TypedActionResult;
 
 public class MobBuddiesClient implements ClientModInitializer {
@@ -59,7 +46,7 @@ public class MobBuddiesClient implements ClientModInitializer {
 	private void openBookUI() {
 		MinecraftClient client = MinecraftClient.getInstance();
 		if (client != null) {
-			client.setScreen(new MyScreen()); // Open UI when player joins
+			client.setScreen(new BookScreenA()); // Open UI when player joins
 		}
 	}
 }
