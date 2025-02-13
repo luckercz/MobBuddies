e063b07098eaadaf5bcdb3e03b06455496c38da1
Lukáš Andrýsek <lukas.andrysek@gmail.com>
2025-02-13 18:56:18 +0100

    feat: Add first working book page

--- src/client/java/com/lucker/mobbuddies/MyScreen.java
+++ src/client/java/com/lucker/mobbuddies/MyScreen.java
@@ -1,20 +1,20 @@
 package com.lucker.mobbuddies;
 
 import io.wispforest.owo.ui.base.BaseOwoScreen;
-import io.wispforest.owo.ui.base.BaseUIModelScreen;
-import io.wispforest.owo.ui.component.ButtonComponent;
 import io.wispforest.owo.ui.component.Components;
 import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
 import io.wispforest.owo.ui.core.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
 
 public class MyScreen extends BaseOwoScreen<FlowLayout> {
+
+    // Path to the custom background image
+    private static final Identifier BACKGROUND_TEXTURE = Identifier.of("mob-buddies", "textures/gui/book_page.png");
 
     @Override
     protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
@@ -23,24 +23,59 @@ public class MyScreen extends BaseOwoScreen<FlowLayout> {
 
     @Override
     protected void build(FlowLayout rootComponent) {
+
+        FlowLayout layout = (FlowLayout) Containers.horizontalFlow(Sizing.content(), Sizing.content())
+                .verticalAlignment(VerticalAlignment.CENTER)
+                .horizontalAlignment(HorizontalAlignment.CENTER);
+
+        //Define Components
+        Component backgroundComponent = Components.texture(BACKGROUND_TEXTURE, 0, 0, 256, 256)
+                .sizing(Sizing.fixed(300), Sizing.fixed(200));
+
+        Component leftButtonComponent =  Containers.verticalFlow(Sizing.content() /**/, Sizing.content())
+                .child((Component) Components.button(Text.literal("   Summon   "), button -> {
+                    // Get current client player
+                    ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+                    // Send command
+                    player.networkHandler.sendChatCommand("specialsummon zombie-buddy");
+                }))
+                .padding(Insets.of(4)) //
+                .surface(Surface.DARK_PANEL)
+                .verticalAlignment(VerticalAlignment.CENTER)
+                .horizontalAlignment(HorizontalAlignment.CENTER)
+                .sizing(Sizing.fixed(75), Sizing.fixed(20))
+                .positioning(Positioning.relative(23,86));
+
+        Component rightButtonComponent =  Containers.verticalFlow(Sizing.content() /**/, Sizing.content())
+                .child((Component) Components.button(Text.literal("   Summon   "), button -> {
+                    // Get current client player
+                    ClientPlayerEntity player = MinecraftClient.getInstance().player;
+
+                    // Send command
+                    player.networkHandler.sendChatCommand("specialsummon skeleton-buddy");
+                }))
+                .padding(Insets.of(4)) //
+                .surface(Surface.DARK_PANEL)
+                .verticalAlignment(VerticalAlignment.CENTER)
+                .horizontalAlignment(HorizontalAlignment.CENTER)
+                .sizing(Sizing.fixed(75), Sizing.fixed(20))
+                .positioning(Positioning.relative(77,86));
+
+        //Define screen using Components
         rootComponent
                 .surface(Surface.VANILLA_TRANSLUCENT)
                 .horizontalAlignment(HorizontalAlignment.CENTER)
                 .verticalAlignment(VerticalAlignment.CENTER);
 
-        rootComponent.child(
-                Containers.verticalFlow(Sizing.content() /**/, Sizing.content())
-                        .child((Component) Components.button(Text.literal("Spawn Zombie"), button -> {
-                            // Get current client player
-                            ClientPlayerEntity player = MinecraftClient.getInstance().player;
+        layout.child(backgroundComponent);
+        layout.child(leftButtonComponent);
+        layout.child(rightButtonComponent);
 
-                            // Send command
-                            player.networkHandler.sendChatCommand("specialsummon zombie-buddy");
-                        }))
-                        .padding(Insets.of(10)) //
-                        .surface(Surface.DARK_PANEL)
-                        .verticalAlignment(VerticalAlignment.CENTER)
-                        .horizontalAlignment(HorizontalAlignment.CENTER)
-        );
+        rootComponent.child(layout);
+
+//        rootComponent.child(backgroundComponent);
+//        rootComponent.child(leftButtonComponent);
+//        rootComponent.child(rightButtonComponent);
     }
 }
